c69cab7b1a062b9d50f766396cdd79f6256e5da4
homeonline23-cloud <homeonline23@gmail.com>
2026-03-09 21:59:54 +0100

    Update mitrax_core.py

--- mitrax_core.py
+++ mitrax_core.py
@@ -1,3 +1,5 @@
+import streamlit as st  # <--- THE MASTER KEY!
+
 # --- THE IMPERIAL BACK DOOR LEVER ---
 if 'step' not in st.session_state: 
-    st.session_state.step = 'sector3'  # <--- Change this to 'intro' when you go live!
+    st.session_state.step = 'sector3'
